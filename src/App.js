3fbbf2e88bd2ff2ac7548328e47254f233d253b6
Athena Chua <athenachua@Athenas-MacBook-Air.local>
2022-05-27 12:49:35 +0800

    create FE repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
+import React, {Component} from 'react';
+import Home from './home';
+import Login from './login';
+import Register from './register';
+import Game from './game';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/hangman" element={<Home/>} />
+        <Route path="/hangman/play" element={<Game/>} />
+      </Routes>
     </div>
   );
 }
